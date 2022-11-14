2e4f879dbd69cd597495f6764c287afcd49d980f
Shishir Paudel <sishirpaudel7@gmail.com>
2022-11-14 16:29:42 +0545

    product details

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,16 +1,17 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Typography } from "@mui/material";
 import LandingPage from "../pages/landingPage/LandingPage";
 import AudioPage from "../pages/audioPage/AudioPage";
+import Product from "../pages/product/Product";
 
 const Router = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<LandingPage/>} />
-            <Route path="/audio" element={<AudioPage/>} />
-            <Route path="/shop" element={<Typography>Hello Universe</Typography>} />
-        </Routes>
-    );
-}
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/audio" element={<AudioPage />} />
+      <Route path="/shop" element={<Product />} />
+    </Routes>
+  );
+};
 
-export default Router
+export default Router;
